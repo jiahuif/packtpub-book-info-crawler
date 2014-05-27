8930dcf15f2832b8fcd97ff37d4f88b1578fe80d
Indeed <indeed@indeedblog.net>
2014-05-27 20:27:31 +0800

    can now save data into sqlite database

--- packtpub-book-info-crawler.py
+++ packtpub-book-info-crawler.py
@@ -6,30 +6,52 @@ import sys
 session = requests.Session()
 
 def crawl_single_book(book_url):
-  try:
-    r = session.get(book_url)
-    r.raise_for_status()
-    page_html = r.text
-    doc = pq(page_html)
-    block = doc(".overview_right .inner")
-    block('h3').remove()
-    book_info_array = map (lambda a: map(lambda s: s.strip() , pq(a).text().split(':')) , block.html().split('<br/>'))
-    book_info = dict(book_info_array)
-    return book_info
-  except:
-    print >> sys.stderr, "Error processing: " + book_url
-page = 0
-while True:
-  listing_url = "http://www.packtpub.com/books?page=%d" % page
-  print >> sys.stderr, listing_url
-  r = session.get(listing_url)
+  r = session.get(book_url)
   r.raise_for_status()
-  listing_html = r.text
-  doc = pq(listing_html)
-  doc.make_links_absolute(listing_url)
-  link_elements = doc(".content .view-content .views-field-title a[href$='book']")
-  links = link_elements.map(lambda i , e: pq(e).attr('href'))
-  for link in links:
-    book_info = crawl_single_book(link)
-    print book_info['ISBN']
-  page += 1
+  page_html = r.text
+  doc = pq(page_html)
+  block = doc(".overview_right .inner")
+  block('h3').remove()
+  book_info_array = map (lambda a: map(lambda s: s.strip() , pq(a).text().split(':')) , block.html().split('<br/>'))
+  book_info = dict(book_info_array)
+  book_info['Title'] = doc('#content-header h1').text()
+  return book_info
+
+def init_database(db_file = 'database.db'):
+  conn = sqlite3.connect(db_file)
+  cur = conn.cursor()
+  cur.execute('CREATE TABLE IF NOT EXISTS books(isbn text PRIMARY KEY, isbn13 text UNIQUE, title text, author text, releaseDate text)')
+  conn.commit()
+  return conn
+
+def save_book_info_to_db(conn, book_info):
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM books WHERE isbn = ?" , (book_info['ISBN'] , ))
+  if len(cur.fetchall()):
+    return
+  cur.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?)", (book_info['ISBN'] , book_info['ISBN 13'] , book_info['Title'] , book_info['Author(s)'] , book_info['Release Date']) )
+  conn.commit()
+conn = init_database()
+try:
+  page = 0
+  while True:
+    listing_url = "Listing url:","http://www.packtpub.com/books?page=%d" % page
+    print >> sys.stderr, listing_url
+    r = session.get(listing_url)
+    r.raise_for_status()
+    listing_html = r.text
+    doc = pq(listing_html)
+    doc.make_links_absolute(listing_url)
+    link_elements = doc(".content .view-content .views-field-title a[href$='book']")
+    links = link_elements.map(lambda i , e: pq(e).attr('href'))
+    for link in links:
+      while True:
+        try:
+          book_info = crawl_single_book(link)
+          save_book_info_to_db(conn, book_info)
+          break
+        except Exception as ex:
+          print >> sys.stderr, "Failed to process url:", link, ex, "Retrying..."
+    page += 1
+finally:
+  conn.close()
